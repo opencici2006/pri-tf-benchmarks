9bc013cc90788fea1668461d4de5832b8316873e
Yixing Lao <yixing.lao@gmail.com>
2017-08-21 14:26:42 -0700

    simplify

--- cnn_util.py
+++ cnn_util.py
@@ -12,19 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
 """Utilities for CNN benchmarks."""
 
 import tensorflow as tf
 
 
 def tensorflow_version_tuple():
-  v = tf.__version__
-  major, minor, patch = v.split('.')
-  return (int(major), int(minor), patch)
+    v = tf.__version__
+    major, minor, patch = v.split('.')
+    return (int(major), int(minor), patch)
 
 
 def tensorflow_version():
-  vt = tensorflow_version_tuple()
-  return vt[0] * 1000 + vt[1]
-
+    vt = tensorflow_version_tuple()
+    return vt[0] * 1000 + vt[1]
--- trivial_model.py
+++ trivial_model.py
@@ -18,12 +18,12 @@ import model
 
 
 class TrivialModel(model.Model):
-  """Trivial model configuration."""
+    """Trivial model configuration."""
 
-  def __init__(self):
-    super(TrivialModel, self).__init__('trivial', 224 + 3, 32, 0.005)
+    def __init__(self):
+        super(TrivialModel, self).__init__('trivial', 224 + 3, 32, 0.005)
 
-  def add_inference(self, cnn):
-    cnn.reshape([-1, 227 * 227 * 3])
-    cnn.affine(1)
-    cnn.affine(4096)
+    def add_inference(self, cnn):
+        cnn.reshape([-1, 227 * 227 * 3])
+        cnn.affine(1)
+        cnn.affine(4096)
