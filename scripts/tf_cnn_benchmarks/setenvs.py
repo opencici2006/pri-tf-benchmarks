fae4e41aa53ff5c90c027d5ed65cbf2a960d7dbb
Sheng Fu <shengfugao@hotmail.com>
2017-09-07 14:42:38 -0700

    turn on env. variable TF_ADJUST_HUE_FUSED to speedup data distortion

--- scripts/tf_cnn_benchmarks/setenvs.py
+++ scripts/tf_cnn_benchmarks/setenvs.py
@@ -165,4 +165,10 @@ def setenvs(inpargv):
         else:
           os.environ["OMP_NUM_THREADS"]= "144"
 
+    # TF_ADJUST_HUE_FUSED will skip expensive data conversion from RGB2HSV and
+    # then HSV2RGB. The fused funciton is NOT implemented on GPU, it is 
+    # off by default. Since this benchmark is running on CPU, we should always 
+    # turn it on
+    os.environ["TF_ADJUST_HUE_FUSED"] = "1"
+    
     return args
