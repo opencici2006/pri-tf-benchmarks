c36c21b53a2f3e8cf6d19e10170a627945379e47
Mahmoud Abuzaina <mahmoud.abuzaina@intel.com>
2017-11-28 11:00:21 -0800

    Syncing with google master branch

--- scripts/tf_cnn_benchmarks/mkl_tf_cnn_benchmarks.py
+++ scripts/tf_cnn_benchmarks/mkl_tf_cnn_benchmarks.py
@@ -37,8 +37,6 @@ args = arglist()
 flags.define_flags()
 for name in flags.param_specs.keys():
   absl_flags.declare_key_flag(name)
-
-
 def main(positional_arguments):
   global args
   args = setenvs(sys.argv)
