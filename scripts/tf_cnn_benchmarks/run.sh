88cc2a26ea0f85929766b97a2b1a8eda2ad8caa0
shaohua <shaohua.zhang@intel.com>
2018-01-10 13:54:24 +0800

    update data format
    Signed-off-by: shaohua <shaohua.zhang@intel.com>

--- scripts/tf_cnn_benchmarks/run.sh
+++ scripts/tf_cnn_benchmarks/run.sh
@@ -1,1 +1,1 @@
-python run_single_node_benchmark.py  -m resnet50 -c bdw -o 44 -b 128 --forward_only true
+python run_single_node_benchmark.py -f NHWC -m resnet50 -c bdw -o 44 -b 128 --forward_only true
